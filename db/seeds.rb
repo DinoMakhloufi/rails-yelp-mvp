b274ff84b59fe4ff6a54cbb95af879fabed038e2
Dino Makhloufi <dino.makhloufi@gmail.com>
2019-08-14 15:22:08 +0200

    Exo pratiquement done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Au bon francais',
+    address:      'rue de la france 75008 Paris',
+    phone_number: '0556345673',
+    category:     'french'
+  },
+  {
+    name:         'Dragon Ball',
+    address:      'rue des boules de cristal 75006 Paris',
+    phone_number: '0556345676',
+    category:     'japanese'
+  },
+  {
+    name:         'Le camembert',
+    address:      'route du bonheur 75016 Paris',
+    phone_number: '0556345678',
+    category:     'french'
+  },
+  {
+    name:         'Chang',
+    address:      'rue de Chine 75013 Paris',
+    phone_number: '0556345677',
+    category:     'chinese'
+  },
+  {
+    name:         'Peponi',
+    address:      'rue de italie 75009 Paris',
+    phone_number: '0556345679',
+    category:     'italian'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
